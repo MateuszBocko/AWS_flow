eef9146c9cd48b4efc573706f5c89453bdb1c37a
mateu <mateuszbocko1@gmail.com>
2023-07-14 12:23:47 +0200

    Adding vpc, python packages

--- modules/RDS/main.tf
+++ modules/RDS/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "lambda_sg" {
     from_port        = 5432
     to_port          = 5432
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"] #pozmieniac na source i zobaczyc czy dziala
   }
 
   egress {
